074ff651513e8d4168604b90749a32966fbafec9
Spirit <83898528+spiritualnulll@users.noreply.github.com>
2024-06-28 18:09:09 +0400

    Update install.cmd
    Add `python -m` prefix

--- install.cmd
+++ install.cmd
@@ -1,33 +1,32 @@
-pip install aiohttp==3.7.4.post0
-pip install anyio==3.6.2
-pip install async-timeout==3.0.1
-pip install attrs==22.2.0
-pip install certifi==2022.12.7
-pip install chardet==4.0.0
-pip install charset-normalizer==3.0.1
-pip install click==8.1.3
-pip install colorama==0.4.
-pip install discord-ext-slash==0.9.2
-pip install discord-webhook==1.0.0
-pip install discord.py==1.7.3
-pip install Flask==2.2.2
-pip install h11==0.14.0
-pip install httpcore==0.16.3
-pip install httpx==0.23.3
-pip install idna==3.4
-pip install importlib-metadata==6.0.0
-pip install itsdangerous==2.1.2
-pip install Jinja2==3.1.2
-pip install MarkupSafe==2.1.2
-pip install multidict==6.0.4
-pip install py-cord
-pip install requests==2.28.2
-pip install rfc3986==1.5.0
-pip install sellpass==1.0
-pip install sniffio==1.3.0
-pip install tls-client==0.1.8
-pip install typing_extensions==4.4.0
-pip install urllib3==1.26.14
-pip install Werkzeug==2.2.3
-pip install yarl==1.8.2
-pip install zipp==3.13.0
+python -m pip install aiohttp==3.7.4.post0
+python -m pip install anyio==3.6.2
+python -m pip install async-timeout==3.0.1
+python -m pip install attrs==22.2.0
+python -m pip install certifi==2022.12.7
+python -m pip install chardet==4.0.0
+python -m pip install charset-normalizer==3.0.1
+python -m pip install click==8.1.3
+python -m pip install colorama==0.4.
+python -m pip install discord-ext-slash==0.9.2
+python -m pip install discord-webhook==1.0.0
+python -m pip install Flask==2.2.2
+python -m pip install h11==0.14.0
+python -m pip install httpcore==0.16.3
+python -m pip install httpx==0.23.3
+python -m pip install idna==3.4
+python -m pip install importlib-metadata==6.0.0
+python -m pip install itsdangerous==2.1.2
+python -m pip install Jinja2==3.1.2
+python -m pip install MarkupSafe==2.1.2
+python -m pip install multidict==6.0.4
+python -m pip install py-cord
+python -m pip install requests==2.28.2
+python -m pip install rfc3986==1.5.0
+python -m pip install sellpass==1.0
+python -m pip install sniffio==1.3.0
+python -m pip install tls-client==0.1.8
+python -m pip install typing_extensions==4.4.0
+python -m pip install urllib3==1.26.14
+python -m pip install Werkzeug==2.2.3
+python -m pip install yarl==1.8.2
+python -m pip install zipp==3.13.0
